7b413b348afd9573463b3f18cc242a1315f86f06
isabellamcamachol <isabellamcamachol@gmail.com>
2023-07-19 00:52:00 -0400

    added decoder to Joseph encoder

--- Joseph_Lab06_encodef.py
+++ Joseph_Lab06_encodef.py
@@ -9,6 +9,15 @@ def encoder(string):
     return encoded_string
 
 
+#Isabella Camacho added
+def decoded(encoded_string):
+    decoded_string = []
+    for ii in encoded_string:
+        for i in range(len(encoded_string)):
+            current = int(encoded_string)
+            new = str(current - 3)
+            decoded_string.append(new)
+    return "".join(decoded_string)
 def main():
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
@@ -17,10 +26,10 @@ def main():
         if menu == 1:
             string = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!\n")
-
-        if menu == 2:
-            pass
-
+            encoded_string =encoder(string)
+        if menu == 2:  #Isabella added menu 2
+            decoded_string = decoded(encoded_string)
+            print(f"The encoded password is {encoded_string}, and the original is {string}\n")
         if menu == 3:
             break
 
