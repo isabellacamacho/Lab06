5ff796beacc4345b7ce4428027a6a1f17b7a4550
Joseph Dominguez <79922707+MrJman32@users.noreply.github.com>
2023-07-19 00:01:08 -0400

    Delete Joseph_Lab06_encodef.py

--- Joseph_Lab06_encodef.py
+++
@@ -1,8 +0,0 @@
-def encoder(string):
-    encoded_string = []
-
-    for ii in string:
-        encoded_char = str((int(ii) + 3) % 10)
-        encoded_string += encoded_char
-
-    return encoded_string
